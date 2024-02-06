3432f12f081e4d8d226ce3b62b7b8bbb790f3de9
OlesiaZaozirna <137696365+OlesiaZaozirna@users.noreply.github.com>
2024-02-06 22:45:34 +0200

    update

---
+++ dist/index.js
@@ -0,0 +1,9 @@
+import { concatenation } from './contacenation';
+const button = document.querySelector('button');
+const input = document.querySelector('input');
+if (button && input) {
+    button.addEventListener('click', () => {
+        concatenation(input.value, 'hello!');
+    });
+}
+//# sourceMappingURL=index.js.map
